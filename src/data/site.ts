2b3860986d1ca8196bcdd48116bcda1f565e9a20
Zachary Crabtree <Zachary.Crabtree7120@gmail.com>
2026-06-23 13:04:42 -0500

    Added Spotlight Page

--- src/data/site.ts
+++ src/data/site.ts
@@ -62,6 +62,7 @@ export const navigation = [
     { name: "Careers", href: "/career" },
     { name: "Status", href: "/status" },
     { name: "Contact", href: "/contact" },
+    { name: "Spotlight", href: "/spotlight" },
 ];
 
 export const services = [
